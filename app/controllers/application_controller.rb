cf2a4efd8b3ae407bf4a12b3f602085c5877699f
Iris <iris.thuo@student.moringaschool.com>
2023-03-04 17:27:04 +0300

    completed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,24 @@
 class ApplicationController < Sinatra::Base
 
   # add routes
+  get '/bakeries' do
+    bakeries = Bakery.all
+    bakeries.to_json
+  end
+
+  get '/bakeries/:id' do
+    bakeries = Bakery.find(params[:id])
+    bakeries.to_json(include: :baked_goods)
+  end
+  
+  get '/baked_goods/by_price' do
+    bakedGoods = BakedGood.all.order(:price)
+    bakedGoods.to_json
+  end
+
+  get '/baked_goods/most_expensive' do
+    bakedGoods = BakedGood.by_price.first
+    bakedGoods.to_json
+  end
 
 end
